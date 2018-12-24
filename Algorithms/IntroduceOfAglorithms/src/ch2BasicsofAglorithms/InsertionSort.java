99a2da4da911c682c1b49f1348809ea3e50cd1e8
cwray01 <kingveyray@gmail.com>
2018-12-24 08:41:55 +0800

    insertSort
    algorithms

--- Algorithms/IntroduceOfAglorithms/src/ch2BasicsofAglorithms/InsertionSort.java
+++ Algorithms/IntroduceOfAglorithms/src/ch2BasicsofAglorithms/InsertionSort.java
@@ -1,35 +1,59 @@
 package ch2BasicsofAglorithms;
 
 public class InsertionSort {
+    public static void revertSort(int array[]){
+        System.out.println("Before Sorting");
+        for (int p=0; p < array.length; p++){
+            System.out.println(array[p]);
+        }
+        System.out.println("==============");
+        int key = 0;
+        for(int j=1; j < array.length; j++){
+            key = array[j];
+            int i = j-1;
+            while (i>=0 && array[i]<key){
+                array[i+1] = array[i];
+                i = i-1;
+                array[i+1] = key;
+            }
+        }
+
+        System.out.println("After Sorting");
+        for(int p=0; p<array.length;p++){System.out.println(array[p]);}
+
+
+    }
     public static void main(String[] args){
         //定义数组
         int A[] = {5,2,4,6,1,3};
-        int B[];
+        int B[] = {31,41,59,26,41,58};
         int key = 0;
 
-        System.out.println("Before Sorting");
+//        System.out.println("Before Sorting");
         //print the array before sorting
-        for(int p=0; p< A.length; p++){
-            System.out.println(A[p]);
+        for(int p=0; p< B.length; p++){
+//            System.out.println(A[p]);
         }
         
-        System.out.println("key: " + key);
-        System.out.println("A[0] " + A[0]);
-        System.out.println("==============");
+//        System.out.println("key: " + key);
+//        System.out.println("B[0] " + B[0]);
+//        System.out.println("==============");
 
-        for(int j=1; j < A.length; j++){
-            key = A[j];
+        for(int j=1; j < B.length; j++){
+            key = B[j];
             int i = j-1;
-            while (i>=0 && A[i]>key){
-                A[i+1] = A[i];
+            while (i>=0 && B[i]>key){
+                B[i+1] = B[i];
                 i = i-1;
-                A[i+1] = key;
+                B[i+1] = key;
             }
         }
 
         //print the array after sorting
-        System.out.println("After Sorting");
-        for(int p=0; p<A.length;p++){System.out.println(A[p]);}
+//        System.out.println("After Sorting");
+//        for(int p=0; p<B.length;p++){System.out.println(B[p]);}
+
+        revertSort(B);
     }
 
 }
